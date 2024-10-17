c66950f7b39b05c67e297df262d6dc9717fb5608
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-10-17 22:14:53 +0300

    install prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt", "@nuxtjs/i18n"],
+  srcDir: "./src",
+  dir: {
+    pages: "pages",
+  },
+  i18n: {},
 });
